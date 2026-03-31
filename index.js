6fb573f3033a7f47f63053eb53cc991c7fe3f8fa
donasher-47f <ckipchirchirasher@gmail.com>
2026-03-31 12:27:57 +0300

    Add attendee object and attendee helper functions

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-//Write your code here
+const attendee = {
+  attendeeId: 'T001',
+  name: 'Alice Smith',
+  event: 'JavaScript Conference',
+  ticketType: 'VIP',
+  ticketPrice: 150.00
+};
 
+function logAttendeeName(attendeeObject) {
+  console.log(attendeeObject.name);
+}
 
+function logTicketPrice(attendeeObject) {
+  console.log(attendeeObject.ticketPrice);
+}
+
+function updateTicketType(attendeeObject, newTicketType) {
+  attendeeObject.ticketType = newTicketType;
+}
+
+function updateTicketPrice(attendeeObject, newTicketPrice) {
+  attendeeObject.ticketPrice = newTicketPrice;
+}
+
+function removeEventProperty(attendeeObject) {
+  delete attendeeObject.event;
+}
+
+function addCheckedInProperty(attendeeObject) {
+  attendeeObject.checkedIn = true;
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
